566ae2c42420faeca919e156b8fa1cfdeccee4e0
Y77GutHub <2235315386@qq.com>
2019-07-10 10:56:20 +0800

    first_commit

---
+++ chapter1Hello JavaScript!/dome1_4helloword.js
@@ -0,0 +1,12 @@
+/*
+	函数名：hello
+	作者:YTT
+	hello函数将输出“hello,Word!"
+ */
+function hello(){
+		//向世界问好
+		var msg="Hello,<em>World!</em>";
+		document.open();
+		document.write(msg);
+		document.close();
+	}
